7b7e459b68484240a8bb60d3648833627d17c873
zokikizz <zp.zoki95@gmail.com>
2020-09-14 12:42:45 +0200

    add to what TODO message to BE side

--- search-me/src/search-me/shared/blog-builder-tags.ts
+++ search-me/src/search-me/shared/blog-builder-tags.ts
@@ -11,6 +11,7 @@ const BLOCKTYPES = [
     'CREATIONDATE',
     'MODIFIEDDATE',
 ];
+// TODO: add BLOGIMAGE and MINICONTENT (generated content from actual content for blogs (just first 265 chars taken))
 
 export const BLOG_BLOCKS: IBlocks = BLOCKTYPES.reduce((acc, current) => {
     acc[current.toLocaleLowerCase()] = new Block(current);
